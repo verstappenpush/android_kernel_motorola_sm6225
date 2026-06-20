845e26e7697058eee20e4ec1d8d51d539165e861
Thiago <thigo6617@gmail.com>
2026-06-20 18:21:59 +0000

    fixup vmscan

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -187,13 +187,13 @@ int kswapd_threads_current = DEF_KSWAPD_THREADS_PER_NODE;
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 160;
+int vm_swappiness = 60;
 
 #ifdef CONFIG_OPLUS_MM_HACKS
 /*
  * Direct reclaim swappiness, values range from 0 .. 160. Higher means more swappy.
  */
-int direct_vm_swappiness = 160;
+int direct_vm_swappiness = 60;
 #endif /* CONFIG_OPLUS_MM_HACKS */
 
 /*
